81ba63451df36551f13c1762bb08c589098cf3c9
mtle <mtle@users.noreply.github.com>
2014-12-05 20:17:32 -0500

    Create uniquePaths.java

---
+++ uniquePaths.java
@@ -0,0 +1,21 @@
+/***
+A robot is located at the top-left corner of a m x n grid (marked
+‘Start’ in the diagram below). The robot can only move either down
+or right at any point in time. The robot is trying to reach the
+bottom-right corner of the grid (marked ‘Finish’ in the diagram below).
+How many possible unique paths are there?
+*/
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class uniquePaths
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	}
+}

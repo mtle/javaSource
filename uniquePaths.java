c986691c9c08f52090af2740875f9650a87a073c
mtle <mtle@users.noreply.github.com>
2014-12-05 20:51:32 -0500

    Update uniquePaths.java

--- uniquePaths.java
+++ uniquePaths.java
@@ -14,8 +14,27 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class uniquePaths
 {
+	public static final int r = 2;
+	public static final int c = 2;
+	
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		System.out.println("(" + r + "," + c + ")" + backtrack(r, c) );
+	}
+	public static int bt(int x, int y)
+	{
+		if (x==r && y==c) return 1;
+		else if (x>r || y>c) return 0;
+		else return bt(x+1,y) + bt(x,y+1);
+	}
+	public static int backtrack(int r, int c)
+	{
+		return bt(0,0);
+	}
+	public static int bt_dp (int x, int y)
+	{
+		Vector<Vector<Integer>> v = new Vector<Vector<Integer>>(r+2, c+2);
+		//Vector<Integer>[] v = new Vector<Integer>[c];
 	}
 }
